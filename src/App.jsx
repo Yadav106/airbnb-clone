895ec91f38c89db70617998ba773c1927fa6d45b
Yadav106 <sahil.jp.yadav@gmail.com>
2023-01-10 11:04:31 +0530

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import MyComponent from './components/MyComponent'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <MyComponent />
     </div>
   )
